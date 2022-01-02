fa26931f39bcef485913548ca5ff0d36d032eec2
Carlos A. Martinez Jimenez <carlos.martinezj@softtek.com>
2022-01-02 12:32:03 -0600

    scribeutil file was removed and new files (datatool and logtracker) were added

--- src/scribeutil/logtracker.py
+++ src/scribeutil/logtracker.py
@@ -1,14 +1,11 @@
 # Python's Libraries
-import re
 import logging
 
-# Own's Libraries
-from errors import InvalidValueError
 
 loggers = {}
 
 
-class ScribeUtil(object):
+class LogTracker(object):
 
     @classmethod
     def get_Logger(self, _logger_name, _with_time=False):
@@ -35,24 +32,6 @@ class ScribeUtil(object):
             loggers[_logger_name] = logger
 
             return logger
-
-    @classmethod
-    def validate_Email(self, _value):
-        """This function validate if a value provided is valid email
-
-        :param _value: value to be validated
-        :type _value: str
-        :raises InvalidValueError: If the value provide is not a valid email
-        """
-
-        regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
-
-        if re.fullmatch(regex, _value) is None:
-            raise InvalidValueError(
-                f"{_value} is not a email value valid",
-                _error="INVALID_EMAIL"
-            )
-
 
 # MSGS LEVELS:
 # - CRITICAL    50
